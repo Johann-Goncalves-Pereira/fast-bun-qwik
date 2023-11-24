5aacc3f98927d4db401423f64a05375e5ba58d45
Johann Goncalves Pereira <johann.gon.pereira@pm.me>
2023-11-24 16:34:13 -0300

    feat: config files

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,23 @@ import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+import postcss from "./postcss.config.js";
+
+
 export default defineConfig(() => {
   return {
     plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    server: {
+      host: "0.0.0.0",
+      port: 5123,
+    },
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
       },
+    },
+    css: {
+      postcss,
     },
   };
 });
